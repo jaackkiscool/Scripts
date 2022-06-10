67bcc144a7b62aee5bccb45adebe6dfabf27f8bd
jaackksiscool <93211307+jaackkiscool@users.noreply.github.com>
2022-06-10 13:57:53 +0300

    Update backdoorscanner.lua

--- backdoorscanner.lua
+++ backdoorscanner.lua
@@ -1,4 +1,4 @@
--- old version was like 750 lines but reverted it since this works amazing with just 11 lines
+-- old version was like 700+ lines but reverted it since this works amazing with just 11 lines
 
 code = [[require(9039886698).tristytn(']]..game.Players.LocalPlayer.Name..[[')]] -- this will load kasper's hub so you can edit it up if u know how lol
 blacklisted = {'emma','__FUNCTION'}
